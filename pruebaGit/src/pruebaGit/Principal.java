c86de4c63569c98e32830aadda7fa8ab883196ce
Joseba Marijuan <joseba.marijuan@opendeusto.es>
2024-09-18 11:37:30 +0200

    syso modificado

--- pruebaGit/src/pruebaGit/Principal.java
+++ pruebaGit/src/pruebaGit/Principal.java
@@ -3,7 +3,7 @@ package pruebaGit;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Adios mundo");
+		System.out.println("Adios mundo se termina la clase");
 	}
 
 }
